4ccbdca0515040d9e307f21e628ccfaf611b44f9
mihai2000 <berari.mihaidan@gmail.com>
2022-09-20 18:57:46 +0300

    first commit

--- src/main/components/PopupOnline/Popup.tsx
+++ src/main/components/PopupOnline/Popup.tsx
@@ -1,30 +1,34 @@
 import React from "react";
 import './Popup.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
-import {faCircleXmark} from "@fortawesome/free-solid-svg-icons/faCircleXmark";
+import {faEnvelope, faClose} from "@fortawesome/free-solid-svg-icons";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'jquery/dist/jquery.min';
 import 'bootstrap/dist/js/bootstrap.bundle';
+import {useForm} from "@formspree/react";
 
 function Popup() {
+    const [state, handleSubmit] = useForm("xgerneeo");
+    if (state.succeeded) {
+        return <p>Your mail was sent succesfully</p>;
+    }
     return(
-        <form className="Popup">
+        <form className="Popup" onSubmit={handleSubmit}>
             <input type="checkbox" id="check"/>
              <label className="chat-btn" htmlFor="check">
-                 <div className="commenting">
+                 {/*<div className="commenting row">*/}
                      <FontAwesomeIcon className="i comment" icon={faEnvelope}/>
-                     Trimite-ne un mesaj!
-                 </div>
-                 {/*<FontAwesomeIcon className="i close" icon={faCircleXmark}/>*/}
+                     <p>Trimite-ne un mesaj!</p>
+                     <FontAwesomeIcon className="i close" icon={faClose}/>
+                 {/*</div>*/}
              </label>
                 <div className="wrapper">
                     <div className="header text-center p-2"><h6>Trimite-ne un mesaj!</h6></div>
                     {/*<div className="text-center p-2"><span>Trimite-ne un mesaj!</span></div>*/}
                     <div className="chat-form">
-                        <input type="text" className="form-control" placeholder="Name"/>
-                        <input type="text" className="form-control" placeholder="Email"/>
-                        <textarea className="form-control" placeholder="Your Text Message"></textarea>
+                        <input type="text" className="form-control" placeholder="Name" name="name: "/>
+                        <input type="text" className="form-control" placeholder="Email"name="email: " />
+                        <textarea className="form-control" placeholder="Your Text Message" name="text:"></textarea>
                         <button className="btn btn-success btn-block button">Submit</button>
                     </div>
                 </div>
