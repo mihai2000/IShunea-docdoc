cfad24c3589dd1964f042a5d0e126e0b8f33e4c6
mihai2000 <berari.mihaidan@gmail.com>
2022-09-22 13:39:36 +0300

    22.09.2022 searchbar

--- src/main/pages/home-page/DocInfo/DocInfo.tsx
+++ src/main/pages/home-page/DocInfo/DocInfo.tsx
@@ -11,21 +11,21 @@ function DocInfo({t}) {
         <section className="doc-info-sistem">
             <div className="container">
                 <div className="row doc-info-sistem">
-                    <div className="col-lg-4 col-sm-6 col-sm-12 .col-xs-12 positions">
+                    <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12 positions">
                         <div className="doc-info-sistem__icon-block">
                             <img src={iconpositions} alt="iconpositions"/>
                             <h1>{t('DocInfo.location.title')}</h1>
                         </div>
                         <h2>{t('DocInfo.location.text')}</h2>
                     </div>
-                    <div className="col-lg-4 col-sm-6 col-sm-12 .col-xs-12 rating">
+                    <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12 rating">
                         <div className="doc-info-sistem__icon-block">
                             <img src={iconrating} alt="iconrating"/>
                             <h1>{t('DocInfo.rating.title')}</h1>
                         </div>
                         <h2>{t('DocInfo.rating.text')}</h2>
                     </div>
-                    <div className="col-lg-4 col-sm-offset-3 col-lg-offset-0 col-sm-6 col-sm-12 .col-xs-12 price">
+                    <div className="col-lg-4 col-sm-offset-3 col-lg-offset-0 col-md-6 col-sm-12 col-xs-12 price">
                         <div className="doc-info-sistem__icon-block">
                             <img src={iconprice} alt="iconprice"/>
                             <h1>{t('DocInfo.pret.title')}</h1>
